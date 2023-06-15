7c494e71377833ba977ee0bd660e74ead9fe35d3
이남곤 <nglee.dev@gmail.com>
2023-06-15 21:59:04 +0900

    feat: PhotoDto, ProductDto

---
+++ src/main/java/com/daangn/daangn/product/dto/PhotoDto.java
@@ -0,0 +1,21 @@
+package com.daangn.daangn.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+public class PhotoDto {
+
+    @AllArgsConstructor
+    @Getter
+    public static class Get {
+
+    }
+
+
+    @AllArgsConstructor
+    @Getter
+    public static class Post {
+
+    }
+    
+}
